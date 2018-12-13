34f56f34571ca66d551ece663581de6a03fc5cd7
ola fijalkowska <aleksandra.kuzniak@gmail.com>
2018-12-13 13:02:48 +0100

    nowy projekt MTAS

---
+++ MTAS/src/DetectorConstruction.cc
@@ -0,0 +1,81 @@
+//
+// $Id: DetectorConstruction.cc 12.16.2016 A. Fijalkowska $
+//
+/// \file DetectorConstruction.cc
+/// \brief DetectorConstruction class
+//
+//
+#include "DetectorConstruction.hh"
+#include "G4PVPlacement.hh" //tworzenie physical volume
+#include "G4SystemOfUnits.hh" //jednostki
+#include "G4VisAttributes.hh" //sposob wyświetlania, kolory itp
+#include "G4Material.hh" //materiały
+#include "G4Box.hh" //prostopadłościan
+#include "G4Tubs.hh" //walec
+#include "G4ThreeVector.hh" //trzyelementowy wektor wbudowany w geant
+#include "globals.hh"
+#include "G4MultiFunctionalDetector.hh"
+#include "G4SDManager.hh"
+#include "G4PSEnergyDeposit.hh"
+
+
+DetectorConstruction::DetectorConstruction()
+{
+    worldLogic = 0L;
+    man = G4NistManager::Instance();
+}
+
+
+
+DetectorConstruction::~DetectorConstruction() {}
+
+
+G4VPhysicalVolume* DetectorConstruction::Construct()
+{
+    G4VPhysicalVolume* worldPhys = ConstructWorld();
+    ConstructNaIDetector();
+    return worldPhys;
+}
+
+
+G4VPhysicalVolume* DetectorConstruction::ConstructWorld()
+{
+
+    G4double worldX = 1.5*m;
+    G4double worldY = 1.5*m;
+    G4double worldZ = 1.5*m;
+    G4Material* vaccum = new G4Material("GalacticVacuum", 1., 1.01*g/mole,
+                           CLHEP::universe_mean_density, 
+                           kStateGas, 3.e-18*pascal, 2.73*kelvin);
+  
+    G4Box* worldSolid = new G4Box("worldSolid",worldX,worldY,worldZ);
+    worldLogic = new G4LogicalVolume(worldSolid, vaccum,"worldLogic", 0,0,0);
+                                             
+    //worldLogic->SetVisAttributes(G4VisAttributes::Invisible);
+    G4VPhysicalVolume* worldPhys = new G4PVPlacement(0, G4ThreeVector(), worldLogic, "world", 0, false, 0);
+    return worldPhys;
+
+}
+
+
+
+
+
+void DetectorConstruction::ConstructNaIDetector()
+{
+
+}
+
+
+
+	
+void DetectorConstruction::ConstructSDandField() 
+{
+
+}
+
+
+
+
+
+
---
+++ MTAS/src/EventAction.cc
@@ -0,0 +1,38 @@
+
+// $Id: EventAction.cc 15.10.2018 A. Fijalkowska $
+//
+/// \file EventAction.cc
+/// \brief Implementation of the EventAction class
+//
+//
+#include "EventAction.hh"
+#include "SteppingAction.hh"
+#include "globals.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4SDManager.hh"
+#include "G4THitsMap.hh"
+
+EventAction::EventAction()
+{
+}
+ 
+EventAction::~EventAction()
+{
+
+}
+
+
+void EventAction::BeginOfEventAction(const G4Event* anEvent)
+{
+
+}
+ 
+
+void EventAction::EndOfEventAction(const G4Event* anEvent)
+{
+
+  
+}
+
+
+
