0960c2fa7b29907b7fc21fa1d787f32de50f94e5
ola fijalkowska <aleksandra.kuzniak@gmail.com>
2018-12-20 14:10:08 +0100

    skrypt do rysowania histogramów

---
+++ MTAS/PlotHisto.cxx
@@ -0,0 +1,26 @@
+void PlotHisto()
+{
+    //nazwa pliku:
+    TFile* file = new TFile("file.root");
+    //nazwa frzewa 
+    TTree* tree=(TTree*)file->Get("treeName");
+ 
+    //zakres histogramu
+    double xMin = 0;
+    double xMax = 5000;//do ustalenia 
+    //liczba binów
+    double xNBins = (xMax-xMin);    
+    
+       
+    TH1F* histo = new TH1F("histo0","histo0",xNBins,xMin,xMax);
+    //nazwaLiscia >> nazwaHistogramu
+    tree->Draw("leafName>>histo0");
+    
+    int lowBin = 100;//threshold
+    int highBin = (int) NBins;//ostani bin
+    double calka = histo->Integral(lowBin, highBin);
+    std::cout << "l. zliczen powyzej progu: " << calka << std::endl;
+    //rysowanie histogramu
+    histo->Draw();
+    
+}
